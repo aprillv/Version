8e90e127d75a4920640f28b45a34be9431415906
aprillv <April@Aprils-Mac-mini.local>
2015-12-08 14:51:02 +0800

    create

---
+++ Version/UpdVersionViewController.swift
@@ -0,0 +1,60 @@
+//
+//  UpdVersionViewController.swift
+//  Version
+//
+//  Created by April on 12/4/15.
+//  Copyright © 2015 HapApp. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class UpdVersionViewController: UIViewController {
+    
+    var info :[String : String]?
+    @IBOutlet weak var Vname: UILabel!
+    @IBOutlet weak var Vvalue: UITextField!
+    @IBAction func updVersion(sender: UIButton) {
+        let a = ["xname":"\(info!["xname"]!)","version":"\(Vvalue.text!)"]
+        //        print(a)
+        Alamofire.request(.POST,
+            "http://ssl.buildersaccess.com/bacontract_UpdVersion.json",
+            parameters: a).responseJSON{ (response) -> Void in
+                //                self.refreshControl?.endRefreshing()
+                if response.result.isSuccess {
+                    //
+                    //                    if let rtnValue = response.result.value {
+                    //                        print(rtnValue)
+                    ////
+                    //                    }else{
+                    //
+                    //                    }
+                    self.navigationController?.popViewControllerAnimated(true)
+                }
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Update"
+        Vname.text = info!["xname"]! + " == " + info!["xvalue"]!
+        Vvalue.text = info!["xvalue"]
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
---
+++ Version/VersionListViewController.swift
@@ -0,0 +1,110 @@
+//
+//  VersionListViewController.swift
+//  Version
+//
+//  Created by April on 12/4/15.
+//  Copyright © 2015 HapApp. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class VersionListViewController: UITableViewController, UISearchBarDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getVersionList()
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        refreshControl?.beginRefreshing()
+        getVersionList()
+        
+    }
+    @IBOutlet weak var searchBar: UISearchBar!
+    var versionOrigin : [[String: String]]? {
+        didSet{
+            searchBar.text = ""
+            versionList = versionOrigin
+        }
+    }
+    var versionList : [[String: String]]? {
+        didSet{
+            tableView.reloadData()
+        }
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        versionList = versionOrigin?.filter(){
+        return $0["xname"]!.lowercaseString.containsString(searchText)
+        }
+    }
+    @IBAction func refresh() {
+        self.getVersionList()
+        
+    }
+    func getVersionList(){
+        
+        let a = ["version" : "0"]
+        Alamofire.request(.POST,
+            "http://ssl.buildersaccess.com/bacontract_VersionList.json",
+            parameters: a).responseJSON{ (response) -> Void in
+                self.refreshControl?.endRefreshing()
+                if response.result.isSuccess {
+                    
+                    if let rtnValue = response.result.value as? [[String: String]]{
+                        self.versionOrigin = rtnValue
+                    }else{
+                        
+                    }
+                    
+                }
+        }
+        
+    }
+   
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        if let v = versionList {
+        return v.count
+        }else{
+        return 0
+        }
+        
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+        let a: [String : String] = versionList![indexPath.row]
+        cell.textLabel?.text = "\(a["xname"]!) == \( a["xvalue"]!)"
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let a: [String : String] = versionList![indexPath.row]
+//        cell.textLabel?.text = "\(a["xname"]!) == \( a["xvalue"]!)"
+        performSegueWithIdentifier("showUpdPage", sender: a)
+    }
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let identifier = segue.identifier
+        if identifier == "showUpdPage" {
+            if let con = segue.destinationViewController as? UpdVersionViewController {
+                if let a = sender as? [String : String]{
+                    con.info = a
+                }
+                
+            }
+        }
+    }
+}
